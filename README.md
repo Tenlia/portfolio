e4d5ebee65f6b00315012ebbbc2bb2a9ee3ce377
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-07-13 12:11:37 -0700

    added the js to get articles from an object

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # portfolio
+
+Fonts from Google Fonts: https://fonts.google.com/
+
+Permanent Marker font by Font Diner : https://fonts.google.com/specimen/Permanent+Marker
+<link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet">
+font-family: 'Permanent Marker', cursive;
+
+Bangers font by Verona Adams : https://fonts.google.com/specimen/Bangers
+<link href="https://fonts.googleapis.com/css?family=Bangers" rel="stylesheet">
+font-family: 'Bangers', cursive;
+
+Oswald font by Verona Adams : https://fonts.google.com/specimen/Oswald
+<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+font-family: 'Oswald', sans-serif;
---
+++ js/articles.js
@@ -0,0 +1,34 @@
+var ourArticles = [
+  {
+    title: 'Lushootseed',
+    authors: 'Alison Zerbe, Aziza Houni, Derek Hewitt, Elizabeth Kleinschmidt',
+    pubDate: '2016-06-26',
+    lastUpdate: '2016-07-13',
+    link: 'https://github.com/AlisonZ/lushootseed',
+    about: 'Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail. Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration.',
+  },
+  {
+    title: 'Bus Mall',
+    authors: 'Elizbeth Kleinschmidt',
+    pubDate: '2016-06-27',
+    lastUpdate: '2016-07-04',
+    link: 'https://github.com/Tenlia/bus-mall',
+    about: 'Capitalise on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.',
+  },
+  {
+    title: 'Salmon Cookies',
+    authors: 'Elizabeth Kleinschmidt, Dylan Holmes',
+    pubDate: '2016-06-20',
+    lastUpdate: '2016-06-27',
+    link: 'https://github.com/Tenlia/cookie_stand',
+    about: 'Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.',
+  },
+  {
+    title: 'About Me',
+    authors: 'Elizabeth Kleinschmidt, Alison Zerbe',
+    pubDate: '2016-06-14',
+    lastUpdate: '2016-06-22',
+    link: 'https://github.com/Tenlia/about_me',
+    about: 'Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.',
+  }
+];
