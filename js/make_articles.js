463681b57012c9fe3ac10999b6a1ed2a31b93602
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-07-15 11:33:52 -0700

    starting on the handlebars

--- js/make_articles.js
+++ js/make_articles.js
@@ -10,21 +10,26 @@ function Entry(input) {
 }
 
 Entry.prototype.toHtml = function() {
-  var $newEntry = $('article.template').clone();
-  $newEntry.removeClass('template').addClass('blog_entry');
+  // var $newEntry = $('article.template').clone();
+  // $newEntry.removeClass('template').addClass('blog_entry');
 
-  $newEntry.find('title').text(this.title);
-  $newEntry.find('.authors').text(this.authors);
-  $newEntry.find('.repo').attr('href', this.link);
-  $newEntry.find('.about').text(this.about);
+  // $newEntry.find('title').text(this.title);
+  // $newEntry.find('.authors').text(this.authors);
+  // $newEntry.find('.repo').attr('href', this.link);
+  // $newEntry.find('.about').text(this.about);
 
-  $newEntry.attr('data-authors', this.authors);
+  // $newEntry.attr('data-authors', this.authors);
 
-  $newEntry.find('time[pubdate]').attr('title', this.pubDate);
-  $newEntry.find('time.pub_date').html('about ' + parseInt((new Date() - new Date(this.pubDate))/60/60/24/1000) + ' days ago');
-  $newEntry.find('time[update]').attr('title', this.lastUpdate);
-  $newEntry.find('time.update').html('about ' + parseInt((new Date() - new Date(this.lastUpdate))/60/60/24/1000) + ' days ago');
-  return($newEntry);
+  // $newEntry.find('time[pubdate]').attr('title', this.pubDate);
+  // $newEntry.find('time.pub_date').html(parseInt((new Date() - new Date(this.pubDate))/60/60/24/1000) + ' days ago');
+  // $newEntry.find('time[update]').attr('title', this.lastUpdate);
+  // $newEntry.find('time.update').html(parseInt((new Date() - new Date(this.lastUpdate))/60/60/24/1000) + ' days ago');
+
+  var source = $('#article-template').html();
+  var content = Handlebars.compile(source);
+  return content(this);
+
+  // return($newEntry);
 };
 
 ourArticles.sort(function(a,b) {
