b5a41b2225a7ac51b1df986b40acb81b1d6e9a01
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-07-15 13:33:52 -0700

    omg, I feel like such an idiot, I broke it because I was missing the script tag for handlebars

--- js/articles.js
+++ js/articles.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var ourArticles = [
   {
     title: 'Lushootseed',
--- js/make_articles.js
+++ js/make_articles.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var allArticles = [];
 
 function Entry(input) {
@@ -10,26 +12,16 @@ function Entry(input) {
 }
 
 Entry.prototype.toHtml = function() {
-  // var $newEntry = $('article.template').clone();
-  // $newEntry.removeClass('template').addClass('blog_entry');
-
-  // $newEntry.find('title').text(this.title);
-  // $newEntry.find('.authors').text(this.authors);
-  // $newEntry.find('.repo').attr('href', this.link);
-  // $newEntry.find('.about').text(this.about);
-
-  // $newEntry.attr('data-authors', this.authors);
-
-  // $newEntry.find('time[pubdate]').attr('title', this.pubDate);
-  // $newEntry.find('time.pub_date').html(parseInt((new Date() - new Date(this.pubDate))/60/60/24/1000) + ' days ago');
-  // $newEntry.find('time[update]').attr('title', this.lastUpdate);
-  // $newEntry.find('time.update').html(parseInt((new Date() - new Date(this.lastUpdate))/60/60/24/1000) + ' days ago');
-
   var source = $('#article-template').html();
-  var content = Handlebars.compile(source);
-  return content(this);
 
-  // return($newEntry);
+  this.publishDaysAgo = parseInt((new Date() - new Date(this.pubDate))/60/60/24/1000);
+  this.publishStatus = this.pubDate ? 'Published ' + this.publishDaysAgo + ' days ago' : '(in progress)';
+  this.updateDaysAgo = parseInt((new Date() - new Date(this.lastUpdate))/60/60/24/1000);
+  this.updateStatus = this.lastUpdate ? 'Updated ' + this.updateDaysAgo + ' days ago' : '(not updated)';
+
+  var content = Handlebars.compile(source);
+  
+  return content(this);
 };
 
 ourArticles.sort(function(a,b) {
@@ -40,6 +32,6 @@ ourArticles.forEach(function(obj) {
   allArticles.push(new Entry(obj));
 });
 
-allArticles.forEach(function(a) {
-  $('#articles').append(a.toHtml());
+allArticles.forEach(function(entry) {
+  $('#articles').append(entry.toHtml());
 });
