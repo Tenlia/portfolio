79d8c109880b5ff32ba6d7786e058d44831bec8f
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-07-29 08:42:59 -0700

    made my changes to server.js

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 
 var express = require('express'),
+		requestProxy = require('express-request-proxy'),
 		port = process.env.PORT || 3000,
 		app = express();
+
+var proxyGitHub = function(request, response) {
+	(
+		requestProxy({
+			url:'https://api.github.com/*' + request.params[0],
+			headers: { Authorization: 'token ' + process.env.GITHUB_TOKEN }
+		})
+	)(request, response);
+};
 
 app.use(express.static('./'));
 
@@ -13,3 +23,5 @@ app.get('*', function(request, response) {
 app.listen(port, function(){
 	console.log('Server started on port ' + port + '!');
 });
+
+
